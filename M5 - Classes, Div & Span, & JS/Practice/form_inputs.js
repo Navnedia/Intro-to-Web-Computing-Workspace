b53ec45ef159c0fde32c814af604070d70b6f1a0
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-04-23 18:38:45 -0500

    Added simple math calculator form

--- M5 - Classes, Div & Span, & JS/Practice/form_inputs.js
+++ M5 - Classes, Div & Span, & JS/Practice/form_inputs.js
@@ -1,7 +1,6 @@
 function alertColor() {
     var firstName = document.getElementById("txtFirstName").value;
     var lastName = document.getElementById("txtLastName").value;
-
     var userFavColor = document.getElementById("selFavColor").value;
 
     alert("Hi " + firstName + " " + lastName + "! My name is Aiden V.");
@@ -11,4 +10,35 @@ function alertColor() {
     } else {
         alert("I see you like the color " + userFavColor + ". My favorite color is blue!");
     }
+}
+
+function doMath() {
+    var num1 = document.getElementById("num1").valueAsNumber;
+    var num2 = document.getElementById("num2").valueAsNumber;
+    var operator = document.getElementById("selMathOperation").value;
+    var result;
+
+    if (isNaN(num1)) {num1 = 0;}
+    if (isNaN(num2)) {num2 = 0;}
+    
+    switch (operator) {
+        case "+":
+            result = num1 + num2;
+
+            break;
+        case "-":
+            result = num1 - num2;
+
+            break;
+        case "*":
+            result = num1 * num2;
+        
+            break;
+        case "/":
+            result = num1 / num2;
+
+            break;
+    }
+
+    alert(num1 + " " + operator + " " + num2 + " = " + result);
 }
