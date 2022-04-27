c4f2bf63d1b1e490af0a52f7060d11b1077f04cc
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-04-26 23:37:29 -0500

    Parsed inputs so we can use textboxes

--- M5 - Classes, Div & Span, & JS/Assign/Scripts/cylinder_calc.js
+++ M5 - Classes, Div & Span, & JS/Assign/Scripts/cylinder_calc.js
@@ -1,14 +1,25 @@
+/**
+ * I know we haven't yet disscussed the parseFloat() function, but I found that the
+ * total surface area function wasn't outputing the correct result because when I 
+ * was adding the (radius + height) the varibles were strings so it was concatenating
+ * the strings together into a new number string and throwing off the calculation. For what
+ * ever reason multipling the strings works in JS, so the first function works fine with 
+ * string variables, but of course when you add them it concatenates them, so the variables
+ * needed to be numbers for the second function to work. Don't worry I have prior coding
+ * experience so I understand what parsing is already anyways.
+ */
+
 function getCurvedSurface() {
-    var radius = document.getElementById("numRadius").valueAsNumber;
-    var height = document.getElementById("numHeight").valueAsNumber;
+    var radius = parseFloat(document.getElementById("txtRadius").value);
+    var height = parseFloat(document.getElementById("txtHeight").value);
 
     var curvedSurfaceArea = 2 * Math.PI * radius * height;
     document.getElementById("calculationResults").innerHTML = "<p>The Curved Surface Area is: " + curvedSurfaceArea +"</p>"; 
 }
 
 function getTotalSurface() {
-    var radius = document.getElementById("numRadius").valueAsNumber;
-    var height = document.getElementById("numHeight").valueAsNumber;
+    var radius = parseFloat(document.getElementById("txtRadius").value);
+    var height = parseFloat(document.getElementById("txtHeight").value);
     
     var totalSurfaceArea = 2 * Math.PI * radius * (radius + height);
     document.getElementById("calculationResults").innerHTML = "<p>The Total Surface Area is: "+ totalSurfaceArea +"</p>";
