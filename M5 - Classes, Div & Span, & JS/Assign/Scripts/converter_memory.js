770b0912645fb9130215addf8214eee35e2298b8
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-04-30 15:18:38 -0500

    Added parseFloat()

--- M5 - Classes, Div & Span, & JS/Assign/Scripts/converter_memory.js
+++ M5 - Classes, Div & Span, & JS/Assign/Scripts/converter_memory.js
@@ -1,6 +1,6 @@
 function ozToGrams() {
-    var ounces = document.getElementById("txtOunces").value;
+    var ounces = parseFloat(document.getElementById("txtOunces").value);
     var grams = ounces * 28.35;
-
+    
     document.getElementById("conversionResults").innerHTML = "<p>Result in Grams: "+ grams +"</p>";
 }
