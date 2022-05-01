f1c2e97a93d2ec5c65a8cd90b1091a1a44b3cc77
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-05-01 00:46:34 -0500

    Created functions for m6 assignment forms

--- M6 - Concatenation, Parsing, FTP/assign/scripts/m6_form_handler.js
+++ M6 - Concatenation, Parsing, FTP/assign/scripts/m6_form_handler.js
@@ -4,11 +4,15 @@ function solveMiniEquation() {
     var num3 = parseFloat(document.getElementById("txtThirdNum").value);
     var answer = (num1 + num2) * num3;
 
-    var output = "<p>The sum of " + num1 + " and " + num2 + 
-                 ", multiplied by " + num3 + " is: " + answer +"</p>"
-    document.getElementById().innerHTML = output;
+    var output = "<p>The sum of " + num1 + " and " + num2 
+               + ", multiplied by " + num3 + " is: " + answer +"</p>"
+    document.getElementById("calculationResult").innerHTML = output;
 }
 
 function summarizeUser() {
+    var name = document.getElementById("txtName").value;
+    var favColor = document.getElementById("selFavColor").value;
 
+    var output = "<p>Your name is: " + name + " and your favorite color is: " + favColor +".</p>";
+    document.getElementById("userInfo").innerHTML = output;
 }
