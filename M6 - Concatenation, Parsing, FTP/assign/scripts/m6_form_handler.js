b67487de4be0b7c485147c8dbc534d0585b62af3
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-05-01 00:27:40 -0500

    Building mini calc code

--- M6 - Concatenation, Parsing, FTP/assign/scripts/m6_form_handler.js
+++ M6 - Concatenation, Parsing, FTP/assign/scripts/m6_form_handler.js
@@ -1,5 +1,12 @@
 function solveMiniEquation() {
-    
+    var num1 = parseFloat(document.getElementById("txtFirstNum").value);
+    var num2 = parseFloat(document.getElementById("txtSecondNum").value);
+    var num3 = parseFloat(document.getElementById("txtThirdNum").value);
+    var answer = (num1 + num2) * num3;
+
+    var output = "<p>The sum of " + num1 + " and " + num2 + 
+                 ", multiplied by " + num3 + " is: " + answer +"</p>"
+    document.getElementById().innerHTML = output;
 }
 
 function summarizeUser() {
