a3188abec07d2fb58af20e7660c643fe220c62a1
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-04-30 22:10:45 -0500

    Updated file structures and names for consistancy

--- M5 - Classes, Div & Span, & JS/Practice/Scripts/form_inputs.js
+++ M5 - Classes, Div & Span, & JS/Practice/Scripts/form_inputs.js
@@ -13,14 +13,16 @@ function alertColor() {
 }
 
 function doMath() {
-    var num1 = document.getElementById("num1").valueAsNumber;
-    var num2 = document.getElementById("num2").valueAsNumber;
+    var num1 = parseFloat(document.getElementById("txtNum1").value);
+    var num2 = parseFloat(document.getElementById("txtNum2").value);
     var operator = document.getElementById("selMathOperation").value;
     var result;
 
+    // Make sure that if the value was left empty that we set the defaults to 0:
     if (isNaN(num1)) {num1 = 0;}
     if (isNaN(num2)) {num2 = 0;}
     
+    // conduct operation based on the operator:
     switch (operator) {
         case "+":
             result = num1 + num2;
