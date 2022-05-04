ca455cfa779cce8642fba42c079166b6c0015859
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-05-03 21:21:29 -0500

    Fixed to not log page reloads as clicks

--- Random Experiments/scripts/rand_color.js
+++ Random Experiments/scripts/rand_color.js
@@ -1,14 +1,14 @@
-randColorBG(); // Run on page load.
+randColorBG(false); // Run on page load without logging click.
 
-function randColorBG() {
+function randColorBG(logClick) {
     hexCode = "#" + Math.floor(Math.random() * 16777215).toString(16);
     document.body.style.backgroundColor = hexCode;
-    addClick()
+
+    if (logClick) {addClick();} // This allows us to not log page reloads as clicks.
 }
 
 function addClick() {
     var clicks = parseInt(localStorage.getItem("clicks"));
-    if (isNaN(clicks)) {clicks = -1;}
-
+    if (isNaN(clicks)) {clicks = 0;}
     localStorage.setItem("clicks", ++clicks);
 }
