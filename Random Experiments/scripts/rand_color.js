95b43b3e3e2999a6f7017019aef0030164049815
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-05-04 15:38:35 -0500

    Added reset keyshort cut

--- Random Experiments/scripts/rand_color.js
+++ Random Experiments/scripts/rand_color.js
@@ -1,4 +1,14 @@
 randColorBG(false); // Run on page load without logging click.
+document.onkeydown = KeyPress; // Set keydown function on page lood.
+
+function KeyPress(e) {
+    if (e.keyCode == 67 && e.ctrlKey && e.altKey) { // If keycombo is Ctrl + Alt + C, then ask if they want to rest counter.
+        // Bring up confirmation box to confirm counter reset:
+        if (confirm("You presses Ctrl + Alt + C: Do you want to reset the click counter?")) {
+            clearCounter();
+        }
+    }
+}
 
 function randColorBG(logClick) {
     hexCode = "#" + Math.floor(Math.random() * 16777215).toString(16);
@@ -17,4 +27,9 @@ function addClick() {
 function updateCounter() {
     var clicks = localStorage.getItem("clicks");
     document.getElementById("clickCounter").innerHTML = clicks;
+}
+
+function clearCounter() {
+    localStorage.setItem("clicks", 0);
+    updateCounter();
 }
