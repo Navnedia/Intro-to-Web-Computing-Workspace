55141a419fc325f488bfa7f980c23ba46b03f8cb
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-05-03 19:16:35 -0500

    Moved page load function call into external script

--- Random Experiments/scripts/rand_color.js
+++ Random Experiments/scripts/rand_color.js
@@ -1,3 +1,5 @@
+randColorBG(); // Run on page load.
+
 function randColorBG() {
     hexCode = "#" + Math.floor(Math.random() * 16777215).toString(16);
     document.body.style.backgroundColor = hexCode;
