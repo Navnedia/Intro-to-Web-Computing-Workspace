d40228091714161de010f6ee97003e3297c788dc
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-05-03 20:23:45 -0500

    Storing clicks

--- Random Experiments/scripts/rand_color.js
+++ Random Experiments/scripts/rand_color.js
@@ -3,4 +3,12 @@ randColorBG(); // Run on page load.
 function randColorBG() {
     hexCode = "#" + Math.floor(Math.random() * 16777215).toString(16);
     document.body.style.backgroundColor = hexCode;
+    addClick()
+}
+
+function addClick() {
+    var clicks = parseInt(localStorage.getItem("clicks"));
+    if (isNaN(clicks)) {clicks = -1;}
+
+    localStorage.setItem("clicks", ++clicks);
 }
