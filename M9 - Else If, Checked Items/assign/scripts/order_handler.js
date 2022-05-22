2c691d881d7403a185eaddd3bedb5a06584ef04f
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-05-22 18:27:21 -0500

    Added comments & adjusted white space

--- M9 - Else If, Checked Items/assign/scripts/order_handler.js
+++ M9 - Else If, Checked Items/assign/scripts/order_handler.js
@@ -1,10 +1,8 @@
-// Make the code cleaner & add comments!!!
-
 function processOrder() {
-    var cost;
     var sandwichType = document.getElementById("selSandwichType").value;
+    var cost;
 
-    // Evaluate sandwich choice:
+    // Add cost of sandwich choice:
     if (sandwichType == "vegetarian") {
         cost = 7;
     } else if (sandwichType == "roast beef") {
@@ -13,24 +11,44 @@ function processOrder() {
         cost = 9;
     }
 
+    /**
+     * For the options and discount fields I choose to pull the values from the 
+     * HTML elements, but in a real world scenario I would never do this. Here,
+     * pulling the values from the HTML introduces massive vulnerabilities that
+     * can easily be explotied by modifying the HTML. The only reason I choose to 
+     * pull the HTML values here is to demonstrat the skill we discussed in the
+     * course module.
+     */
+
     // Add cost for additional options:
     if (document.getElementById("chkAvocado").checked) {
-        cost += parseFloat(document.getElementById("chkAvocado").value); // Maybe remove.
+        // Wants Avocado
+        // Get additional cost from HTML element.
+        cost += parseFloat(document.getElementById("chkAvocado").value);
     }
     if (document.getElementById("chkArtisanalBun").checked) {
-        cost += parseFloat(document.getElementById("chkArtisanalBun").value); // Maybe remove.
+        // Wants Artisanal Bun
+        // Get additional cost from HTML element.
+        cost += parseFloat(document.getElementById("chkArtisanalBun").value);
     }
 
-    // Apply Discount if applicable:
+    // Apply a selected discount if applicable:
     if (document.getElementById("radStudent").checked) {
-        cost -= cost * parseFloat(document.getElementById("radStudent").value); // Maybe remove.
-    } else if (document.getElementById("radStaff").checked || document.getElementById("radFaculty").checked) {
-        cost -= cost * parseFloat(document.getElementById("radStaff").value); // Maybe remove.
+        // Subtract student discount of (% off) from cost.
+        // Get % from HTML element.
+        cost -= cost * parseFloat(document.getElementById("radStudent").value);
+    } else if (document.getElementById("radStaff").checked || 
+               document.getElementById("radFaculty").checked) {
+        // Subtract staff OR faculty discount of (% off) from cost.
+        // Get % from HTML element.
+        cost -= cost * parseFloat(document.getElementById("radStaff").value);
     }
 
+    // Round off to two decimals because it's a dollar amount:
     cost = cost.toFixed(2);
 
-    // Maybe seperate out the output string.
-    var output = "<p>You ordered a " + sandwichType + " sandwich. Your total cost is: $" + cost + ".</p>"
+    // Format and display output:
+    var output = "<p>You ordered a " + sandwichType 
+                + " sandwich. Your total cost is: $" + cost + ".</p>";
     document.getElementById("orderSummary").innerHTML = output;
 }
