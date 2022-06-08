9877bf52a073899624add4c185fd440adf0dd635
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-06-07 22:40:59 -0500

    Fixed variable name bug

--- M9 - Else If, Checked Items/practice/scripts/museum_form.js
+++ M9 - Else If, Checked Items/practice/scripts/museum_form.js
@@ -1,9 +1,11 @@
 function calcEntry() {
     // Should I use some values in the form for practice?
+    console.log("hello?");
 
     var fee;
     var today = new Date();
-    var dateStr = (today.getMonth + 1) + "/" + (today.getDate());
+    var dateStr = (today.getMonth() + 1) + "/" + (today.getDate());
+    console.log("dateStr: " + dateStr);
     
     if (document.getElementById("radReg").checked) {
         fee = 10;
@@ -21,7 +23,7 @@ function calcEntry() {
 
     fee = fee.toFixed(2); // Round to 2 decimals for dollar amount.
 
-    var output = "<p>Thank you for your order on " + datestr 
+    var output = "<p>Thank you for your order on " + dateStr 
                + ". Your entry fee is $" + fee 
                + ". Thanks for your support!</p>";
     document.getElementById("admissionFeeResult").innerHTML = output;
