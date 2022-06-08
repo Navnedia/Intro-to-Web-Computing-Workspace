f78ffa0a0f8787f7de1b251928b67023e20d57cc
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-06-07 22:28:31 -0500

    Added basic form functionality

---
+++ M9 - Else If, Checked Items/practice/scripts/museum_form.js
@@ -0,0 +1,28 @@
+function calcEntry() {
+    // Should I use some values in the form for practice?
+
+    var fee;
+    var today = new Date();
+    var dateStr = (today.getMonth + 1) + "/" + (today.getDate());
+    
+    if (document.getElementById("radReg").checked) {
+        fee = 10;
+    } else {
+        fee = 15;
+    }
+
+    if (document.getElementById("chkSenior").checked) {
+        fee -= 5; // Subtract $5 from fee.
+    } // Else do nothing (no senior discount).
+
+    if (document.getElementById("radIsMember").checked) {
+        fee -= fee * 0.1; // Subtract 10% from fee.
+    } // Else do nothing (no member discount).
+
+    fee = fee.toFixed(2); // Round to 2 decimals for dollar amount.
+
+    var output = "<p>Thank you for your order on " + datestr 
+               + ". Your entry fee is $" + fee 
+               + ". Thanks for your support!</p>";
+    document.getElementById("admissionFeeResult").innerHTML = output;
+}
